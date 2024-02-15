2e2acc09159bd5b3bd2f9b70b8d44922d78656e7
eduardd002 <cortespeduard2002@gmail.com>
2024-02-15 12:34:08 -0500

    Se agrega la constante de rutas para hacer las urls de los links mas cortas y amigables.

---
+++ Configuracion/Rutas.php
@@ -0,0 +1,5 @@
+<?php
+
+    define("rutaInicio", "http://localhost/Mercado-Juegos");
+
+?>
--- Index.php
+++ Index.php
@@ -1,53 +1,55 @@
 <?php
 
+    require_once 'Configuracion/Rutas.php';
+
     require_once 'autoload.php';
 
     require_once 'Vistas/Layout/Cabecera.html';
 
-//Mostrar error
+    //Mostrar error
 
-function showError(){
-    $error = new ErrorController();
-    $error -> index();
-    return $error;
-}
+    function showError(){
+        $error = new ErrorController();
+        $error -> index();
+        return $error;
+    }
 
-//Compruebo si me llega el controlador por la URL 
+    //Compruebo si me llega el controlador por la URL 
 
-if(isset($_GET['controller'])){
-    $nombre = $_GET['controller'];
-}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
-    $nombre = "VideojuegoController";
-}else{
-    showError();
-    exit();
-}
-
-//Compruebo si existe el controlador
-
-if(class_exists($nombre)){
-    
-    //Si existe la clase se crea el objeto
-
-    $controlador = new $nombre();
-
-    //Compruebo si me llega la acción y si el metodo existe dentro del controlador
-
-    if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
-
-        //Si existe invocar o llamar a ese metodo
-
-        $action = $_GET['action'];
-        $controlador -> $action();
+    if(isset($_GET['controller'])){
+        $nombre = $_GET['controller'];
     }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
-        $actionDefault = "Inicio";
-        $controlador -> $actionDefault();
+        $nombre = "VideojuegoController";
+    }else{
+        showError();
+        exit();
+    }
+
+    //Compruebo si existe el controlador
+
+    if(class_exists($nombre)){
+        
+        //Si existe la clase se crea el objeto
+
+        $controlador = new $nombre();
+
+        //Compruebo si me llega la acción y si el metodo existe dentro del controlador
+
+        if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+
+            //Si existe invocar o llamar a ese metodo
+
+            $action = $_GET['action'];
+            $controlador -> $action();
+        }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+            $actionDefault = "Inicio";
+            $controlador -> $actionDefault();
+        }else{
+            showError();
+        }
     }else{
         showError();
     }
-}else{
-    showError();
-}
 
     require_once 'Vistas/Layout/PieDePagina.html';
 
