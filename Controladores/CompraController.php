d1b1f483378839558bd67497c9c8667fd0a3f996
eduardd002 <cortespeduard2002@gmail.com>
2024-02-16 23:38:33 -0500

    Se crearon nuevas ventanas, tales como envio, pago y compra exitosa, con su respectiva maquetacion.

---
+++ Controladores/CompraController.php
@@ -0,0 +1,25 @@
+<?php
+
+    class CompraController{
+
+        public function direccion(){
+            //Cargar la vista
+
+            require_once "Vistas/Compra/Envio.html";
+        }
+
+        public function pago(){
+            //Cargar la vista
+
+            require_once "Vistas/Compra/Pago.html";
+        }
+
+        public function exito(){
+            //Cargar la vista
+
+            require_once "Vistas/Compra/Exito.html";
+        }
+
+    }
+
+?>
