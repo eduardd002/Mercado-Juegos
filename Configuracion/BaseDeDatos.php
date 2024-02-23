3408ac1cc94ec6c11b9c0b7ae8c0d433a0202737
eduardd002 <cortespeduard2002@gmail.com>
2024-02-23 13:27:49 -0500

    Se realiza la funcion para conectar la base de datos al proyecto pero aun sin la base de datos creada.

--- Configuracion/BaseDeDatos.php
+++ Configuracion/BaseDeDatos.php
@@ -1,3 +1,21 @@
 <?php
 
+    //Conexion a la base de datos
+
+    class BaseDeDatos{
+        
+        //Funcion para conectar la base de datos al proyecto
+
+        public static function connect() {
+            //Instancia de clase, por parametros se pasan el nombre del servidor, 
+            //el usuario, la clae y el nombre de la base de datos
+            $db = new mysqli('localhost', 'root', '', 'nombre de la base');
+            //Se indica el tipamiento de datos
+            $db -> query("SET NAMES 'utf8'");
+            //Se retorna la base de datos
+            return $db;
+        }
+        
+    }
+
 ?>
